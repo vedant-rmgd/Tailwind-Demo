d95592cc00284e1df2c27bc69492f2e7b8d6ab4d
vedant-rmgd <vedant.ramgade@gmail.com>
2025-05-03 22:51:04 +0530

    add logo

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -5,7 +5,13 @@ function Footer() {
     <div>
         <footer class="bg-black text-white py-6 border-t-2 border-white">
         <div class="max-w-6xl mx-auto px-4 flex flex-col md:flex-row justify-between items-center">
-          <p class="text-sm">&copy; 2025 YourCompany. All rights reserved.</p>
+        <div className="w-11 h-11 rounded-full flex items-center justify-center overflow-hidden">
+          <img
+            className="w-full h-full object-cover"
+            src="https://upload.wikimedia.org/wikipedia/commons/7/76/Physics_wallah_logo.jpg?20211017183441"
+            alt=""
+          />
+        </div>
           <div class="flex space-x-4 mt-4 md:mt-0">
             <a href="#" class="hover:underline">
               Privacy Policy!
