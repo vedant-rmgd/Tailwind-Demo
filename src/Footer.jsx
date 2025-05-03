2b71320b318e315c90d2fd036e44a8893d13ac3b
vedant-rmgd <vedant.ramgade@gmail.com>
2025-05-03 22:56:12 +0530

    add logo and text

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -1,10 +1,17 @@
-import React from 'react'
+import React from "react";
 
 function Footer() {
   return (
     <div>
-        <footer class="bg-black text-white py-6 border-t-2 border-white">
+      <footer class="bg-black text-white py-6 border-t-2 border-white">
         <div class="max-w-6xl mx-auto px-4 flex flex-col md:flex-row justify-between items-center">
+          <div className="w-11 h-11 rounded-full flex items-center justify-center overflow-hidden">
+            <img
+              className="w-full h-full object-cover"
+              src="https://upload.wikimedia.org/wikipedia/commons/7/76/Physics_wallah_logo.jpg?20211017183441"
+              alt=""
+            />
+          </div>
           <p class="text-sm">&copy; 2025 YourCompany. All rights reserved.</p>
           <div class="flex space-x-4 mt-4 md:mt-0">
             <a href="#" class="hover:underline">
@@ -20,7 +27,7 @@ function Footer() {
         </div>
       </footer>
     </div>
-  )
+  );
 }
 
-export default Footer
+export default Footer;
